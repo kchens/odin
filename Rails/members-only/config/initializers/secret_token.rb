31eff79ab5b1fb3f04cda0fa8fbf67746a7682da
kchens <kkch3n@gmail.com>
2014-04-08 17:09:45 -0700

    Starting members-only

---
+++ Rails/members-only/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MembersOnly::Application.config.secret_key_base = 'f0e41bc5ba4736b865e761b9820170ba75314743d64e94cc2dcaadd57033f3855b3dd59e10a607733b699010ef62e82d7a9c6ec37d23ff6d480b112259a052f6'
