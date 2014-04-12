bf8a807f5cba7f3ef27a08c4b051800f0e0f113d
kchens <kkch3n@gmail.com>
2014-04-11 23:10:08 -0700

    ugly, but finished

--- Rails/members-only/app/controllers/posts_controller.rb
+++ Rails/members-only/app/controllers/posts_controller.rb
@@ -1,23 +1,44 @@
 class PostsController < ApplicationController
 	before_action :signed_in_user, only: [:new, :create]
 
+
 	def index
 		@posts = Post.all
 	end
 
 	def new
+		@post = Post.new
 	end
 
 	def create
 		#automatically creates a post with the author's foreign key
-		@post = current_user.posts.build(note: current_user[:id])
-		redirect_to posts_path if @post.save #post/index.html.erb
+
+		@post = Post.new(post_params)
+		@post.user_id = current_user.id
+		puts "This is #{@post.user_id} user"
+		puts "#{@post.errors.full_messages}"
+		if @post.save #post/index.html.erb
+			redirect_to posts_path
+		else
+			redirect_to new_post_path
+		end
+
+		# Two ways to associate user_id
+		# 1)
+		# @post = Post.new  -- in #new
+		# @post.user_id = current_user.id  -- in #create
+		# 2)
+		# @post = current_user.posts.build  -- in #create
 	end
 
 	def destroy
 	end
 
 	private
+
+	def post_params
+		params.require(:post).permit(:note)
+	end
 
 	def signed_in_user
 		redirect_to signout_path, notice: "Please sign in." unless signed_in?
--- Rails/members-only/app/controllers/sessions_controller.rb
+++ Rails/members-only/app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 	def create
 		user = User.find_by(email: params[:session][:email].downcase)
   		if user && user.authenticate(params[:session][:password])
-			sign_in user
+			sign_in(user)
 			redirect_to root_path
 		else
 			render :new
