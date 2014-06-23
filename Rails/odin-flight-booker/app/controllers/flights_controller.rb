effd8396c2df6e99a2cae9219849ee5b3ebcb100
kchens <kkch3n@gmail.com>
2014-06-23 17:20:27 -0400

    Rewriting repository

--- Rails/odin-flight-booker/app/controllers/flights_controller.rb
+++ Rails/odin-flight-booker/app/controllers/flights_controller.rb
@@ -4,5 +4,7 @@ class FlightsController < ApplicationController
     @airports = Airport.all.map { |x| x.codename }
     @num_passengers = (1..4).map { |x| x }
     @dates = Flight.all.map {|x| x.formatted_date}
+
+    
   end
 end
---
+++ readme.md
@@ -0,0 +1,1 @@
+This repo is a showcase of my TheOdinProject.com work.
